8de8b836c22115992eb6ee931fb258a824190ade
innzeero <innzeero@gmail.com>
2021-04-25 13:22:29 +0900

    java script example

---
+++ 01_data_type.js
@@ -0,0 +1,9 @@
+console.log(1);
+console.log(1+1);
+console.log("Hello World");
+console.log("Hello World".length)
+console.log("Hello World".toUpperCase());
+console.log("Hello World".indexOf('O'));
+console.log("Hello World".indexOf('o'));
+console.log("   Hello World   ".trim())
+
---
+++ 02_variable.js
@@ -0,0 +1,13 @@
+var x = 1;
+var y = 2;
+
+console.log(x+y);
+
+var x = 1000;
+console.log(x+y);
+
+var coffee = "latte"
+console.log("My favorite coffee is "+coffee)
+
+var c = "constant"
+console.log("Number 1 is a "+c)
